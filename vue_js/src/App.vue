c885a927fab00c325f835041c2f69215bc2610e4
Lusine Hovhannisyan <lusine.hovhannisyan@esterox.am>
2023-05-27 20:28:21 +0400

    vue js

---
+++ vue_js/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <my-component />
+  </div>
+</template>
+
+<script>
+import MyComponent from './components/MyComponent'
+
+export default {
+  name: 'App',
+  components: {
+    MyComponent
+  }
+}
+</script>
---
+++ vue_js/src/components/MyComponent.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="root">
+    <label>
+      Введите email:
+      <input
+        type="email"
+        :value="email"
+        @input="emailInput($event.target.value)"
+      />
+    </label>
+    <label>
+      Введите пароль:
+      <input
+        type="password"
+        :value="password"
+        @input="passwordInput($event.target.value)"
+      />
+    </label>
+
+    email: {{ email }}
+    password: {{ password }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyComponent',
+  data() {
+    return {
+      email: 'example@mail.ru',
+      password: '12345678'
+    }
+  },
+  methods: {
+    emailInput(value) {
+      this.email = value
+    },
+    passwordInput(value) {
+      this.password = value
+    }
+  }
+}
+</script>
